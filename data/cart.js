b1bdb23627796d11a479b8f3b408af7cc59e582c
Tutorial <tpanagopulos2@gmail.com>
2024-07-13 14:48:49 +0200

    Save cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,10 +1,19 @@
-export let cart = [{
-  productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-  quantity: 2,
-}, {
-  productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-  quantity: 1
-}];
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if(!cart) {
+  cart = [{
+    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity: 2,
+  }, {
+    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 1
+  }];
+}
+
+
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addToCart(productId) {
   let matchingItem; // Creating matchingItem variable to save the item if it is the same as the one in cart
@@ -22,8 +31,8 @@ export function addToCart(productId) {
       productId,
       quantity: 1
       });  // Else we push the new object(item) in the cart
-    }
-    console.log(cart);
+    };
+    saveToStorage();
   }
 
   // function for removing the product from the cart by using parametr 
@@ -43,4 +52,6 @@ export function addToCart(productId) {
     });
 
     cart = newCart;
-  };
+
+    saveToStorage();
+  }
