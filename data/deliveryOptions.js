c92e19da5f8ee263494fcce43e6aba608bea7d69
Tutorial <tpanagopulos2@gmail.com>
2024-07-15 16:26:50 +0200

    Save data and generate HTML for delivery Options

---
+++ data/deliveryOptions.js
@@ -0,0 +1,18 @@
+/*
+Variable to save all the different delivery options the customer can choose from
+It's purpose is to have these properties below which we will later use in checkout.js
+export is going into checkout.js
+*/
+export const deliveryOption = [{
+  id: '1',
+  deliveryDates: 7,
+  priceCents: 0
+}, {
+  id: '2',
+  deliveryDays: 3,
+  priceCents: 499
+}, {
+  id: '3',
+  deliveryDays: 1,
+  priceCents: 999
+}];
