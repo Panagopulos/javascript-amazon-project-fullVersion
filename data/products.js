ee12c6cc8db8b666346d72bc64dccec4187a25d6
Tutorial <tpanagopulos2@gmail.com>
2024-07-30 21:27:29 +0200

    Load products from the backend

--- data/products.js
+++ data/products.js
@@ -80,11 +80,35 @@ class Clothing extends Product {
   `;
  }
 }
+// Will accept products-data from backend
+export let products = [];
 
+//Function which accepts a callback function as a parametr from checkout.js and amazon.js we use the callback at the end of this code so we first get the response from backend and after that we can load all the data. 
+export function loadProducts(fun) {
+ const xhr = new XMLHttpRequest();
 
+ xhr.addEventListener('load', () => {
+    products = JSON.parse(xhr.response).map((productDetails) => {
+      if (productDetails.type === 'clothing') {
+        return new Clothing(productDetails);
+      }
+     return new Product(productDetails);
+    });
 
+    console.log('load products');
 
+    fun();
+ });
+
+ xhr.open('GET', 'https://supersimplebackend.dev/products');
+ xhr.send();
+}
+
+loadProducts();
+
+/*
 // This is our products array which includes objects of each product with all the necessary properties and since we need each object to be new Instance we use method map() where we loop through all the productDetails(properties as an object) and create new instance out of each object in the array. As well If we have type = clothing we create different instance called Clothing. To differentiate the Clothing products.
+
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -750,3 +774,4 @@ export const products = [
   }
  return new Product(productDetails);
 });
+*/
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,97 +1,102 @@
 import { addToCart, 
   calculateCartQuantity} from '../data/cart.js';
-import { products } from '../data/products.js';
+import { products, loadProducts } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
+
+loadProducts(renderProductsGrid);
 // Variable of array including object which are representing properties for
 // the eshop items so we are able to loop through this array and generate
 // the html for all the different objects(items)
-let productsHTML = '';
+function renderProductsGrid() {
 
-// forEach method connected to 'products' array including objects of shop items in file 'Data/products' 
-products.forEach((product) => {  
-  productsHTML +=  `          
-    <div class="product-container">
-            <div class="product-image-container">
-              <img class="product-image"
-                src="${product.image}">
-            </div>
+  let productsHTML = '';
 
-            <div class="product-name limit-text-to-2-lines">
-              ${product.name}
-            </div>
-
-            <div class="product-rating-container">
-              <img class="product-rating-stars"
-                src="${product.getStarsUrl()}">
-              <div class="product-rating-count link-primary">
-                ${product.rating.count}
+  // forEach method connected to 'products' array including objects of shop items in file 'Data/products' 
+  products.forEach((product) => {  
+    productsHTML +=  `          
+      <div class="product-container">
+              <div class="product-image-container">
+                <img class="product-image"
+                  src="${product.image}">
               </div>
+
+              <div class="product-name limit-text-to-2-lines">
+                ${product.name}
+              </div>
+
+              <div class="product-rating-container">
+                <img class="product-rating-stars"
+                  src="${product.getStarsUrl()}">
+                <div class="product-rating-count link-primary">
+                  ${product.rating.count}
+                </div>
+              </div>
+
+              <div class="product-price">
+                ${product.getPrice()}
+              </div>
+
+              <div class="product-quantity-container">
+                <select>
+                  <option selected value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                  <option value="6">6</option>
+                  <option value="7">7</option>
+                  <option value="8">8</option>
+                  <option value="9">9</option>
+                  <option value="10">10</option>
+                </select>
+              </div>
+
+              ${product.extraInfoHTML()}
+
+              <div class="product-spacer"></div>
+
+              <div class="added-to-cart">
+                <img src="images/icons/checkmark.png">
+                Added
+              </div>
+
+              <button class="add-to-cart-button button-primary js-add-to-cart"
+              data-product-id="${product.id}">
+                Add to Cart
+              </button>
             </div>
-
-            <div class="product-price">
-              ${product.getPrice()}
-            </div>
-
-            <div class="product-quantity-container">
-              <select>
-                <option selected value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
-              </select>
-            </div>
-
-            ${product.extraInfoHTML()}
-
-            <div class="product-spacer"></div>
-
-            <div class="added-to-cart">
-              <img src="images/icons/checkmark.png">
-              Added
-            </div>
-
-            <button class="add-to-cart-button button-primary js-add-to-cart"
-            data-product-id="${product.id}">
-              Add to Cart
-            </button>
-          </div>
-  `;
-});
+    `;
+  });
 
 
-// Using dom to showcase the final html we created on main page.
-document.querySelector('.js-products-grid')
-  .innerHTML = productsHTML;
+  // Using dom to showcase the final html we created on main page.
+  document.querySelector('.js-products-grid')
+    .innerHTML = productsHTML;
 
-  // Making the shopping cart in the top right header interactive.
-  function updateCartQuantity() {
-    
-    const cartQuantity = calculateCartQuantity();
+    // Making the shopping cart in the top right header interactive.
+    function updateCartQuantity() {
+      
+      const cartQuantity = calculateCartQuantity();
 
-    document.querySelector('.js-cart-quantity')
-     .innerHTML = cartQuantity;
-  }
+      document.querySelector('.js-cart-quantity')
+      .innerHTML = cartQuantity;
+    }
 
-  updateCartQuantity();
+    updateCartQuantity();
 
-  // Selecting all 'Add to Cart' button by using DOM and looping through them 
-  // Then adding to each button EventListener so it listen to click and when 
-  // that happens we save the unique ID we get from the data set attribute in
-  // our generated html into 'productId' variable 
-  document.querySelectorAll('.js-add-to-cart')
-    .forEach((button) => {
-      button.addEventListener('click', () => {
-       const productId = button.dataset.productId;
-       
-       addToCart(productId);
-       
-      updateCartQuantity();
+    // Selecting all 'Add to Cart' button by using DOM and looping through them 
+    // Then adding to each button EventListener so it listen to click and when 
+    // that happens we save the unique ID we get from the data set attribute in
+    // our generated html into 'productId' variable 
+    document.querySelectorAll('.js-add-to-cart')
+      .forEach((button) => {
+        button.addEventListener('click', () => {
+        const productId = button.dataset.productId;
         
+        addToCart(productId);
+        
+        updateCartQuantity();
+          
+        });
       });
-    });
+  }
